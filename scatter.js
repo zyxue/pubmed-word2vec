ee4da23a25f251bef1f0520c0e88feae05a99810
Zhuyi Xue <alfred532008@gmail.com>
2017-05-10 09:16:01 -0700

    use color column from csv if available

--- scatter.js
+++ scatter.js
@@ -120,7 +120,6 @@ d3.csv("lele.csv", function(data) {
         .attr("y2", height);
 
     console.log(data.length);
-    // Add Text Labels
     objects.selectAll('.dotlabel')
         .data(data)
         .enter()
@@ -132,7 +131,7 @@ d3.csv("lele.csv", function(data) {
         .attr("transform", transform)
         .attr("font_family", "sans-serif")
         .attr("font-size", "13px")
-        .attr("fill", "darkgreen");
+        .attr("fill", function(d) {return 'color' in d ? d['color'] : 'darkgreen';});
 
     objects.selectAll(".dot")
         .data(data)
